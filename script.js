8d07e0e514f85691481f80ca5d45130b82beb3f4
laissivedovato <laissi.vedovato@gmail.com>
2022-03-17 20:19:21 -0300

    #03 - Implementation of actions completed

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+//array com a lista de musicas e informações
+let musicas = [
+  {titulo: 'Guitar Solo', artista:'Artista 1', src:'./musics/We Ride! - Reed Mathis.mp3', img:'./img/rock.jpg'},
+  {titulo: 'Samba', artista:'Artista 2', src:'./musics/projeto_spotify_parte_1_musicas_Ella Vater - The Mini Vandals.mp3', img:'./img/samba.jpg'},
+  {titulo: 'Piano music', artista:'Artista 3', src:'./musics/A Brand New Start - TrackTribe (1).mp3', img:'./img/piano.jpg'}
+];
+
 let musica = document.querySelector('audio');
+
+let indexMusica = 0;
 
 let duracaoMusica = document.querySelector('.fim');
 
-duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+let imagem = document.querySelector('img');
+
+let nomeMusica = document.querySelector('.descricao h2');
+
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
 
  //EVENTOS
  document.querySelector('.botao-play').addEventListener('click', tocarMusica);
@@ -11,7 +26,33 @@ duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
 
   musica.addEventListener('timeupdate', atualizarBarra);
 
+  document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+      indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+  });
+
+  document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2) {
+      indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+  });
+
 //tocar música
+  function renderizarMusica(index) {
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+      nomeMusica.textContent = musicas[index].titulo;
+      nomeArtista.textContent = musicas[index].artista;
+      imagem.src = musicas[index].img;
+      duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
  function tocarMusica() {
    musica.play();
    document.querySelector('.botao-pause').style.display = 'block';
@@ -42,3 +83,5 @@ function segundosParaMinutos(segundos) {
   }
   return campoMinutos+ ':' +campoSegundos;
 }
+
+
